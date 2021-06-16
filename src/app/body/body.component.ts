a5996623398a9ec33b0a6536cb958f8c2bb98b29
sahil1148 <85242685+sahil1148@users.noreply.github.com>
2021-06-16 09:42:55 +0530

    Add files via upload

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,65 +1,37 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css']
 })
-export class BodyComponent implements OnInit {
-  house: FormGroup
-  house_service: any;
-  constructor(private fb: FormBuilder, ) {  //imported service 
-    this.house = this.fb.group({
-      amount: new FormControl('', Validators.required),
-      start_date: new FormControl('', [Validators.required]),
-      end_date: new FormControl('', Validators.required),
-      acc_type: new FormControl('', Validators.required),
-      landlord_name: new FormControl('', Validators.required),
-      postcode: new FormControl('', Validators.required),
-      property_address: new FormControl('', Validators.required),
-      landlord_address: new FormControl('', Validators.required), //all required validator will be here
-      Council_Tax: new FormControl(''),
-      Electricity_Bill: new FormControl(''),
-      Water_Bill: new FormControl(''),
-      Gas_Bill: new FormControl(''),
-      agree: new FormControl('', Validators.requiredTrue)
+export class BodyComponent{
+  title = 'travel-expense-details';
+  mode_of_transports = ['Bus', 'Train', 'Seaway','Airway'];
+  modes = ['weekly', 'Monthly', 'yearly'];
+  mode_of_transportHasError = true;
+  modeHasError = true;
+  dFrom =true;
+  
 
-
-    });
-  }
-
-  ngOnInit(): void {
-
-  }
-
-
-  submitprompt() {
-    alert("ok")
-  }
-  //houseset(){
-  //  console.log()
-  //}
-  //sending to the db by api
-  public errorMessage: any;
-
-  houseset() {
-    if (this.house.invalid) {
-      return;
+  userModel = new User('','default','default'); 
+  validatemode_of_transport(value: any){
+    if (value === 'default') {
+      this.mode_of_transportHasError = true;
     } else {
-      let form = JSON.stringify(this.house.value);
-      this.house_service.posthouse(form)
-        .subscribe((response: any) => {
-          //Next callback
-          console.log(response)
-          //this.repos = response;
-        },
-          (error: { message: any; }) => {                              //Error callback
-            console.error(error)
-            this.errorMessage = error.message;
-
-          })
+      this.mode_of_transportHasError = false;
     }
   }
+  
 
+validatemode(value: any){
+  if (value === 'default') {
+    this.modeHasError = true;
+  } else {
+    this.modeHasError = false;
+  }
 }
+  
+}
+
